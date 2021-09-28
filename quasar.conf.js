d310418945ec41fbe5a29da60a86ea47aaf05b50
Vicente Olivares <vicenteolivares@MacBook-Pro-de-Vicente.local>
2021-09-28 15:43:17 -0500

    Parse install

--- quasar.conf.js
+++ quasar.conf.js
@@ -12,7 +12,7 @@ module.exports = function(/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
-    boot: ['i18n', 'axios', 'amplify', 'plugins', 'components', 'filters'],
+    boot: ['i18n', 'axios', 'amplify', 'plugins', 'components', 'filters', 'parse'],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ['app.scss'],
---
+++ src/boot/parse.js
@@ -0,0 +1,14 @@
+import Parse from 'parse'
+
+Parse.initialize(
+  "",""
+)
+Parse.serverURL = 'http://143.198.123.252:1337/parse/'
+
+//Dashboard: http://143.198.123.252:4040/apps/BrounieApp/webhooks
+//admin
+//Brounie1A
+
+//Server: http://143.198.123.252:1337/parse/
+//ApplicationId = "brounieapps"
+//masterKey: "brounieKEY"
